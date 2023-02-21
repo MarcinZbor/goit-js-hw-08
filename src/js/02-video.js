b7b6a482edf59bdc7aa2b9a8b0e011e1ab10efe7
MarcinZbor <marcinzb@gazeta.pl>
2023-02-21 20:42:44 +0100

    Full8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+import vimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+const STORAGE_KEY = 'current-time';
+
+const getCurrentTime = ({ seconds }) => {
+  localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+player.on('play', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const KEY_STORAGE = 'feedback - form - state';
+
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onTextInput, 1000));
+
+populateText();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const formData = new FormData(form);
+  const finalData = {};
+  for (const [key, value] of formData.entries()) {
+    if (!value) {
+      alert('Złe dane');
+      return;
+    }
+    finalData[key] = value;
+  }
+  console.log(finalData);
+  form.reset();
+  localStorage.removeItem(KEY_STORAGE);
+}
+
+function onTextInput(event) {
+  const { name, value } = event.target;
+  const parsedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (parsedData) {
+    const formData = {
+      ...parsedData,
+      [name]: value,
+    };
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+  } else {
+    const formData = { [name]: value };
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+  }
+}
+
+function populateText() {
+  const parsedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (parsedData) {
+    const inputNames = Object.keys(parsedData);
+    inputNames.forEach(inputName => {
+      const input = formRef.elements[inputName];
+      input.value = parsedData[inputName];
+    });
+  }
+}
